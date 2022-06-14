e6130ebedea8b206ffce9baf0410c17e00ad25d0
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-14 11:01:56 -0600

    Test error handling if the POST fails

--- cypress/e2e/homepage/errorHandling.cy.js
+++ cypress/e2e/homepage/errorHandling.cy.js
@@ -12,5 +12,24 @@ describe ('Burrito Builder fetch error handling', () => {
 
     cy.get('.load-error').should('have.text', 'Oh no! There was a problem loading the data. Please try again later.')
   });
-  
+
+  it('Should see an error message if the form POST fails', () => {
+    cy.intercept('POST', "http://localhost:3001/api/v1/orders", {
+      statusCode: 500,
+      headers: {
+        'x-requested-with': 'exampleClient',
+      },
+      fixture: 'singleResponse'
+    })
+    cy.visit('http://localhost:3000/')
+
+    cy.get('input[name="name"]').type('Chama').should('have.value', 'Chama')
+    cy.get('#jalapenos').click()
+    cy.get('#beans').click()
+    cy.get('.order-info').should('have.text', 'Order: jalapenos, beans')
+    cy.get('.sub-btn').click()
+
+    cy.get('.post-error').should('have.text', 'Oh no! There was a problem submitting your order. Please try again later!')
+  });
+
 })
