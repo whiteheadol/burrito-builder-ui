b5c01ed2701eb90441a7d11dac032e17ef7710ad
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-14 11:50:23 -0600

    Remove test comments

--- cypress/e2e/homepage/homepage.cy.js
+++ cypress/e2e/homepage/homepage.cy.js
@@ -62,20 +62,6 @@ describe ('Burrito Builder', () => {
     cy.get('#jalapenos').click()
     cy.get('.form-error').should('contain', 'Please include your name and at least one ingredient in your order')
   });
-
-  // it('Should display an error message if the initial fetch call fails', () => {
-  //   cy.intercept('GET', "http://localhost:3001/api/v1/orders", {
-  //     statusCode: 500,
-  //     headers: {
-  //       'x-requested-with': 'exampleClient',
-  //     },
-  //     fixture: 'initialFetch'
-  //   })
-  //   cy.visit('http://localhost:3000/')
-  //
-  //   cy.get('.load-error').should('have.text', 'Oh no! There was a problem loading the data. Please try again later.')
-  //
-  // });
 })
 
 
