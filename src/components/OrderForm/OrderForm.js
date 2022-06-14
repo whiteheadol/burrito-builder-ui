f7ae17da050cec35d0fdebcf352ac43fd540d94c
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-14 09:40:16 -0600

    Add successful post with no error handling

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -10,21 +10,53 @@ class OrderForm extends Component {
     };
   }
 
-
   handleSubmit = e => {
     e.preventDefault();
+    this.postOrder();
     this.clearInputs();
   }
 
+  postOrder = () => {
+    const order = {
+      name: this.state.name,
+      ingredients: this.state.ingredients
+    }
+
+    fetch("http://localhost:3001/api/v1/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(order)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      this.props.addOrder(data)
+    })
+  }
+
+  // second then, push the response into the apps state
+  // if a catch, console log an error message
+
   clearInputs = () => {
     this.setState({name: '', ingredients: []});
+  }
+
+  handleIngredientChange = (e) => {
+    e.preventDefault();
+    this.setState({ ingredients: [...this.state.ingredients, e.target.value]})
+  }
+
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value})
   }
 
   render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} value={ingredient} onClick={e => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
       )
